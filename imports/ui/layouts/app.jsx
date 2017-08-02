f2f03003a8aed7a6a2480cf0700b3a63c35fe182
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2017-08-02 12:14:52 +0100

    Create a simple UI to show the sports and respective data; Create a subscription to keep all data updated in the client-side;

--- imports/ui/layouts/app.jsx
+++ imports/ui/layouts/app.jsx
@@ -1,18 +1,128 @@
 import React from 'react'
+import { Meteor } from 'meteor/meteor'
 import { createContainer } from 'meteor/react-meteor-data'
 
 import Hello from '../components/hello/hello.jsx'
 import Info from '../components/info/info.jsx'
 
+import { Sports } from '../../api/sports'
+
+const BetBlock = (props) => (
+  <p>
+    <span>{props.bet.name}</span>
+    <span> - {props.bet.home.value}</span>
+    {props.bet.draw.name && <span> | {props.bet.draw.value}</span>}
+    <span> | {props.bet.away.value}</span>
+  </p>
+)
+
+const BetsBlock = (props) => (
+  <ol>
+    {props.bets.map(bet => (
+      <li>
+        <BetBlock bet={bet} />
+      </li>
+    ))}
+  </ol>
+)
+
+const EventBlock = (props) => (
+  <div>
+    <p>
+      <span>{props.event.code}</span>
+      <span> - {props.event.date.toLocaleString()}</span>
+      <span> - {props.event.home} X {props.event.away}</span>
+    </p>
+    <BetsBlock bets={props.event.bets} />
+  </div>
+)
+
+const EventsBlock = (props) => (
+  <ol>
+    {props.events.map(event => (
+      <li>
+        <EventBlock event={event} />
+      </li>
+    ))}
+  </ol>
+)
+
+const CompetitionBlock = (props) => (
+  <div>
+    <p>{props.competition.name}</p>
+    <EventsBlock events={props.competition.events} />
+  </div>
+)
+
+const CompetitionsBlock = (props) => (
+  <ol>
+    {props.competitions.map(competition => (
+      <li>
+        <CompetitionBlock competition={competition} />
+      </li>
+    ))}
+  </ol>
+)
+
+const CountryBlock = (props) => (
+  <div>
+    <p>
+      <span>{props.country.name}</span> - <span>{props.country.url}</span>
+    </p>
+    <CompetitionsBlock competitions={props.country.competitions} />
+  </div>
+)
+
+const CountriesBlock = (props) => (
+  <ol>
+    {props.countries.map(country => (
+      <li>
+        <CountryBlock country={country} />
+      </li>
+    ))}
+  </ol>
+)
+
+const SportBlock = (props) => (
+  <div>
+    <p>{props.sport.name}</p>
+    <CountriesBlock countries={props.sport.countries} />
+  </div>
+)
+
+const SportsBlock = (props) => (
+  <ol>
+    {props.sports.map(sport => (
+      <li>
+        <SportBlock sport={sport} />
+      </li>
+    ))}
+  </ol>
+)
+
+const MainBlock = (props) => (
+  <div>
+    <h3>Size: {props.sports.length}</h3>
+    <h3>Sports:</h3>
+    <SportsBlock sports={props.sports} />
+  </div>
+)
+
 const App = (props) => (
   <section>
     <Hello title={props.title} />
     <Info />
+    { !props.loading && <MainBlock sports={props.sports} /> }
   </section>
 )
 
 export default createContainer(props => {
+  const allSportsHandle = Meteor.subscribe('sports.all')
+  const allSports = Sports.getAll()
+
   return {
+    loading: !allSportsHandle.ready(),
+    sports: allSports.fetch(),
     title: 'Welcome to Meteor!'
   }
 }, App)
