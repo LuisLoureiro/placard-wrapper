2000bb08ebc4279b3a103665735ab9150ac8cba1
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2017-08-04 17:17:52 +0100

    Reduce the number of components, there's no need in having so many.

--- imports/ui/layouts/app.jsx
+++ imports/ui/layouts/app.jsx
@@ -4,94 +4,66 @@ import { createContainer } from 'meteor/react-meteor-data'
 
 import { Sports } from '../../api/sports'
 
-const BetBlock = (props) => (
-  <p>
-    <span>{props.bet.name}</span>
-    <span> - {props.bet.home.value}</span>
-    {props.bet.draw.name && <span> | {props.bet.draw.value}</span>}
-    <span> | {props.bet.away.value}</span>
-  </p>
-)
-
 const BetsBlock = (props) => (
   <ol>
     {props.bets.map((bet, idx) => (
       <li key={idx}>
-        <BetBlock bet={bet} />
+        <p>
+          <span>{bet.name}</span>
+          <span> - {bet.home.value}</span>
+          {bet.draw.name && <span> | {bet.draw.value}</span>}
+          <span> | {bet.away.value}</span>
+        </p>
       </li>
     ))}
   </ol>
-)
-
-const EventBlock = (props) => (
-  <div>
-    <p>
-      <span>{props.event.code}</span>
-      <span> - {props.event.date.toLocaleString()}</span>
-      <span> - {props.event.home} X {props.event.away}</span>
-    </p>
-    <BetsBlock bets={props.event.bets} />
-  </div>
 )
 
 const EventsBlock = (props) => (
   <ol>
     {props.events.map((event, idx) => (
       <li key={idx}>
-        <EventBlock event={event} />
+        <p>
+          <span>{event.code}</span>
+          <span> - {event.date.toLocaleString()}</span>
+          <span> - {event.home} X {event.away}</span>
+        </p>
+        <BetsBlock bets={event.bets} />
       </li>
     ))}
   </ol>
-)
-
-const CompetitionBlock = (props) => (
-  <div>
-    <p>{props.competition.name}</p>
-    <EventsBlock events={props.competition.events} />
-  </div>
 )
 
 const CompetitionsBlock = (props) => (
   <ol>
     {props.competitions.map((competition, idx) => (
       <li key={idx}>
-        <CompetitionBlock competition={competition} />
+        <p>{competition.name}</p>
+        <EventsBlock events={competition.events} />
       </li>
     ))}
   </ol>
-)
-
-const CountryBlock = (props) => (
-  <div>
-    <p>
-      <span>{props.country.name}</span> - <span>{props.country.url}</span>
-    </p>
-    <CompetitionsBlock competitions={props.country.competitions} />
-  </div>
 )
 
 const CountriesBlock = (props) => (
   <ol>
     {props.countries.map((country, idx) => (
       <li key={idx}>
-        <CountryBlock country={country} />
+        <p>
+          <span>{country.name}</span> - <span>{country.url}</span>
+        </p>
+        <CompetitionsBlock competitions={country.competitions} />
       </li>
     ))}
   </ol>
-)
-
-const SportBlock = (props) => (
-  <div>
-    <p>{props.sport.name}</p>
-    <CountriesBlock countries={props.sport.countries} />
-  </div>
 )
 
 const SportsBlock = (props) => (
   <ol>
     {props.sports.map((sport, idx) => (
       <li key={idx}>
-        <SportBlock sport={sport} />
+        <p>{sport.name}</p>
+        <CountriesBlock countries={sport.countries} />
       </li>
     ))}
   </ol>
