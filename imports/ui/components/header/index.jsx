baa9eb88973146fc5f328f85ea16bbe11da8a736
Loureiro <LuisLoureiro@users.noreply.github.com>
2017-08-23 17:41:53 +0100

    Add a link to the original Placard website; Fix some styling.

--- imports/ui/components/header/index.jsx
+++ imports/ui/components/header/index.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import Sports from '../sports'
 import Countries from '../countries'
+import PlacardLink from '../placard-link/index'
 
 export default (props) => (
   <header>
     <nav>
+      <PlacardLink />
       {
         !props.loadingSports &&
         <Sports sports={props.sports} hideChildren>
