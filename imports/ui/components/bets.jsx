25299c3f72e82ca104464efe44abfe94231d0362
Loureiro <LuisLoureiro@users.noreply.github.com>
2017-08-28 16:49:42 +0100

    Change some styling in the list of events.

--- imports/ui/components/bets.jsx
+++ imports/ui/components/bets.jsx
@@ -4,12 +4,12 @@ export default (props) => (
   <ol>
     {props.bets.map((bet, idx) => (
       <li key={idx}>
-        <p>
+        <small>
           <span>{bet.name}</span>
           <span> - {bet.home.value}</span>
           {bet.draw.name && <span> | {bet.draw.value}</span>}
           <span> | {bet.away.value}</span>
-        </p>
+        </small>
       </li>
     ))}
   </ol>
