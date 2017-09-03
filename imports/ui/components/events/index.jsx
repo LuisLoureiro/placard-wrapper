62d018cd3b3e853ae3fd71414c4deaa3ddf3b7ac
Loureiro <LuisLoureiro@users.noreply.github.com>
2017-09-04 00:13:07 +0100

    Use pseudo-elements to put a '-' before almost every part of the 'event' paragraph. Improve styling of the 'event' paragraph.

--- imports/ui/components/events/index.jsx
+++ imports/ui/components/events/index.jsx
@@ -10,16 +10,16 @@ export default (props) => (
     {props.events.map((event, idx) => (
       <li key={idx}>
         <p>
-          <span>{event.code}</span>
-          <span> - {event.date && new Date(event.date).toLocaleString()}</span>
-          <span> - {event.home} X {event.away}</span>
+          <span className='bold'>{event.code}</span>
+          <span>{event.date && new Date(event.date).toLocaleString()}</span>
+          <span className='bold'>{event.home} X {event.away}</span>
           {
-            !props.hideSport ? <span> - {event.sport}</span> : ''
+            !props.hideSport ? <span>{event.sport}</span> : ''
           }
           {
-            !props.hideCountry ? <span> - {event.country}</span> : ''
+            !props.hideCountry ? <span>{event.country}</span> : ''
           }
-          <span> - {event.competition}</span>
+          <span>{event.competition}</span>
         </p>
         {cloneChildren(props.children, {
           bets: event.bets
