e8ba1a79fb4be2d36df09ec3211b94732834121d
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2017-09-11 17:43:09 +0100

    Configure client-side init to use a Redux store initialized with data arrived from the server.

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,8 +1,15 @@
 import { Meteor } from 'meteor/meteor'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
 
 import { renderRoutes } from './routes.jsx'
+import withProvider from '../both/withProvider'
+import mainReducer from '../../api/redux/reducers'
 
 Meteor.startup(() => {
-  render(renderRoutes(), document.getElementById('app'))
+  const store = createStore(mainReducer, window.__PRELOADED_STATE__)
+
+  delete window.window.__PRELOADED_STATE__
+
+  render(withProvider(store, renderRoutes()), document.getElementById('app'))
 })
