c5f6fe5821fc47871a313ccbc6d9034f8b9fcd01
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2018-07-31 16:43:26 +0100

    Use a gatsby source plugin for pulling data int app from MongoDB collections;

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    /*
+     * Gatsby's data processing layer begins with “source” plugins. Here we
+     * setup the site to pull data from the "documents" collection in a local
+     * MongoDB instance
+     */
+    {
+      resolve: 'gatsby-source-mongodb',
+      options: {
+        dbName: process.env.MONGODB_NAME,
+        server: {
+          address: process.env.MONGODB_ADDRESS,
+          port: process.env.MONGODB_PORT
+        },
+        auth: {
+          user: process.env.MONGODB_USER,
+          password: process.env.MONGODB_PASS
+        },
+        collection: ['sportsAndCountries', 'events']
+      }
+    }
+  ]
+}
