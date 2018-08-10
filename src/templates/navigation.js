9d53d005760743e19b22f66c3fbec037d1439a2c
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2018-08-10 18:40:26 +0100

    Style "Navigation" component: - display sports inlined; - display countries on mouse hover over sports.

--- src/templates/navigation.js
+++ src/templates/navigation.js
@@ -1,26 +1,33 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+import styles from './navigation.module.styl'
+
 export default ({ sports }) => (
   <nav>
-    <ol>
+    <ol className={styles['sports-list']}>
       {
         sports.map(({ node }, idx) => (
-          <li key={idx}>
-            <NavLink
-              to={`/${node.name}`}
-              activeClassName='active'>
-              { node.name }
-            </NavLink>
-            <ol>
+          <li key={idx} className={`${styles['sport-item']}`}>
+            <h3>
+              <NavLink
+                to={`/${node.name}`}
+                activeClassName='active'>
+                { node.name }
+              </NavLink>
+            </h3>
+            <ol className={`${styles['countries-list']}`}>
               {
                 node.countries.map((country, idx) => (
-                  <li key={idx}>
-                    <NavLink
-                      to={`/${node.name}/${country.name}`}
-                      activeClassName='active'>
-                      { country.name }
-                    </NavLink>
+                  <li key={idx} className={`${styles['country-item']}`}>
+                    <h3>
+                      <NavLink
+                        to={`/${node.name}/${country.name}`}
+                        activeClassName='active'
+                        className={styles.nowrap}>
+                        { country.name }
+                      </NavLink>
+                    </h3>
                   </li>
                 ))
               }
