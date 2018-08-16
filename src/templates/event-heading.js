331013157818afdfc5fe18a6d7fb0d474b40cb45
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2018-08-16 12:22:39 +0100

    Format date and time display.

--- src/templates/event-heading.js
+++ src/templates/event-heading.js
@@ -1,14 +1,46 @@
 import React from 'react'
 
-export default ({ event }) => (
-  <h4>
-    {
-      `${event.code}
-      - ${new Date(event.date * 1000).toLocaleString()}
-      - ${event.home} X ${event.away}
-      - ${event.sport}
-      - ${event.country}
-      - ${event.competition}`
+export default class EventHeading extends React.Component {
+  constructor (props) {
+    super(props)
+
+    const date = new Date(props.event.date * 1000)
+
+    this.state = {
+      dateDate: formatDate(date),
+      dateTime: formatTime(date),
+      eventName: `${props.event.home} X ${props.event.away}`
     }
-  </h4>
-)
+  }
+
+  render () {
+    const event = this.props.event
+
+    return (
+      <h4>
+        {
+          `${event.code}
+          - ${this.state.dateDate}, ${this.state.dateTime}
+          - ${this.state.eventName}
+          - ${event.sport}
+          - ${event.country}
+          - ${event.competition}`
+        }
+      </h4>
+    )
+  }
+}
+
+function formatDate (date) {
+  const now = new Date()
+
+  return date.getDate() === now.getDate() ? 'Hoje'
+    : date.getDate() === (now.getDate() + 1) ? 'Amanhã'
+    : date.toLocaleDateString()
+}
+
+function formatTime (date) {
+  const time = date.toLocaleTimeString()
+  // Remove seconds from return string
+  return time.slice(0, time.lastIndexOf(':'))
+}
