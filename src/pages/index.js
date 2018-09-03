ff49e66bcde3ca944c41c269f67155606f7c34f4
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2018-09-03 16:57:57 +0100

    Create a new Component responsible for handling the logic associated with the page changes and the pagination; This way we have a more cleaner Index component;

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,62 +5,18 @@ import EventHeading from '../templates/event-heading'
 
 import styles from './index.module.styl'
 
-export default class Index extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      pagedEvents: '',
-      pageNumber: 1
+export default ({ data }) => (
+  <ol className={styles['events-list']}>
+    {
+      data.allMongodbPlacardDevEvents.edges.map(({ node }, idx) => (
+        <li key={idx} className={styles['event-item']}>
+          <EventHeading event={node} />
+          <EventBets betTypes={node.betTypes} />
+        </li>
+      ))
     }
-    this.data = props.data.allMongodbPlacardDevEvents.edges
-    this.numberOfPages = props.pathContext.numberOfPages
-    this.url = buildPath(props.pathContext)
-    this.loadMore = this.loadMore.bind(this)
-    this.showLoadMoreButton = this.showLoadMoreButton.bind(this)
-  }
-
-  loadMore () {
-    const self = this
-
-    if (this.state.pageNumber < this.numberOfPages) {
-      return window.fetch(`${this.url}/${this.state.pageNumber + 1}`)
-        .then(response => response.text())
-        .then(getEventsFromText)
-        .then(moreEvents => self.setState({
-          pagedEvents: self.state.pagedEvents.concat(moreEvents),
-          pageNumber: self.state.pageNumber + 1
-        }))
-    }
-  }
-
-  showLoadMoreButton () {
-    return this.state.pageNumber < this.numberOfPages
-  }
-
-  render () {
-    return (
-      <main>
-        <ol className={styles['events-list']}>
-          {
-            this.data.map(({ node }, idx) => (
-              <li key={idx} className={styles['event-item']}>
-                <EventHeading event={node} />
-                <EventBets betTypes={node.betTypes} />
-              </li>
-            ))
-          }
-        </ol>
-        <ol className={styles['events-list']} dangerouslySetInnerHTML={{ __html: this.state.pagedEvents }} />
-        <button type='button'
-          style={{ display: this.showLoadMoreButton() ? 'block' : 'none' }}
-          onClick={this.loadMore}>
-          Carregar mais eventos
-        </button>
-      </main>
-    )
-  }
-}
+  </ol>
+)
 
 export const query = graphql`
   query EventsListQuery($sport: String, $country: String, $skip: Int = 0, $limit: Int = 20) {
@@ -91,13 +47,3 @@ export const query = graphql`
     }
   }
 `
-
-const buildPath = ({sport, country}) => {
-  return `/${sport || ''}${country ? `/${country}` : ''}`
-}
-
-const getEventsFromText = text => {
-  const bodySlice = text.slice(text.indexOf('<body'), text.lastIndexOf('</body>'))
-
-  return bodySlice.slice(bodySlice.indexOf('><li') + 1, bodySlice.lastIndexOf('</li>') + 5)
-}
