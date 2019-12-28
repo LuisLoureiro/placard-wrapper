c8c7025c11e2082b03f9738d1c5fd59e9355ba1a
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2019-12-28 19:28:49 +0000

    Rename boundActionCreators to actions

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 const path = require(`path`)
 
-exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
   const template = path.resolve('./src/templates/index.js')
 
   return new Promise((resolve) => {
