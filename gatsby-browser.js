3b3f3dd37559ff0e96fa27c7192fc81f8b086b00
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2019-12-29 18:30:26 +0000

    Correctly convert to pure ES6.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 import ComponentRenderer from './src/components/component-renderer-with-pagination'
 
-export default {
-  replaceComponentRenderer: ({ props, loader }) => {
-    if (props.layout) {
-      return null
-    }
-
-    return <ComponentRenderer {...props} loader={loader} />
+export const replaceComponentRenderer = ({ props, loader }) => {
+  if (props.layout) {
+    return null
   }
+
+  return <ComponentRenderer {...props} loader={loader} />
 }
