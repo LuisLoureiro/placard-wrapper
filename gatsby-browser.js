b8ccdea2458feba31ff0355c9c38c334b08ba1bb
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2020-01-05 01:33:00 +0000

    Remove old and unnecessary code.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,9 +3,9 @@ import React from 'react'
 import ComponentRenderer from './src/components/component-renderer-with-pagination'
 
 export const replaceComponentRenderer = ({ props, loader }) => {
-  if (props.layout) {
-    return null
-  }
-
   return <ComponentRenderer {...props} loader={loader} />
 }
+
+
+
+
