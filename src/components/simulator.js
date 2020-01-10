c2e2821aab557efed6922ef51f5ba63b0f9b053a
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2020-01-10 00:26:47 +0000

    Transform function-style component into class-styled component; Handle minimize on header click;

--- src/components/simulator.js
+++ src/components/simulator.js
@@ -1,58 +1,93 @@
 import React from 'react'
 import style from './simulator.module.styl'
 
-export default ({
-  selectedBets = [],
-  onSelectedBetsCleared = () => {},
-  onSelectedBetRemoved = () => {}
-}) => (
-  <table className={`${style.simulator} ${style.minimized}`}>
-    <thead>
-      <tr>
-        <th colSpan='2'>
-          <button>
-            <span className={style.toLeft}>SIMULADOR</span>
-            <span className={`${style.rotate315} ${style.toRight}`}>V</span>
-          </button>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      {selectedBets.map(bet => (
-        <tr key>
-          <td>
-            <p className={style.bold}>
-              <small>{bet.code}</small>
-              <small>
-                {bet.home} X {bet.away}
-              </small>
-            </p>
-            <p className={style.alignCenter}>
-              <small>
-                <u>{bet.name}</u>
-              </small>
-              <span>{bet.odd.name}</span>
-              <span className={style.bold}>{bet.odd.value}</span>
-            </p>
-          </td>
-          <td className={style.alignCenter}>
-            <button onClick={() => onSelectedBetRemoved(bet)}>X</button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-    <tfoot>
-      <tr>
-        <td colSpan='2'>
-          <button onClick={onSelectedBetsCleared}>Limpar</button>
-          <span className={style.toRight}>
-            Total:
-            <span className={style.bold}>
-              {selectedBets.reduce((prev, curr) => prev + curr.odd.value, 0)}
-            </span>
-          </span>
-        </td>
-      </tr>
-    </tfoot>
-  </table>
-)
+class Simulator extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { minimized: false }
+
+    this.handleChangedMinimized = this.handleChangedMinimized.bind(this)
+  }
+
+  handleChangedMinimized () {
+    this.setState({ minimized: !this.state.minimized })
+  }
+
+  render () {
+    return (
+      <table
+        className={`${style.simulator} ${
+          this.state.minimized ? style.minimized : ''
+        }`}
+      >
+        <thead>
+          <tr>
+            <th colSpan='2'>
+              <button onClick={this.handleChangedMinimized}>
+                <span className={style.toLeft}>SIMULADOR</span>
+                <span
+                  className={`${
+                    this.state.minimized ? style.rotate225 : style.rotate315
+                  } ${style.toRight}`}
+                >
+                  V
+                </span>
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.selectedBets.map(bet => (
+            <tr key>
+              <td>
+                <p className={style.bold}>
+                  <small>{bet.code}</small>
+                  <small>
+                    {bet.home} X {bet.away}
+                  </small>
+                </p>
+                <p className={style.alignCenter}>
+                  <small>
+                    <u>{bet.name}</u>
+                  </small>
+                  <span>{bet.odd.name}</span>
+                  <span className={style.bold}>{bet.odd.value}</span>
+                </p>
+              </td>
+              <td className={style.alignCenter}>
+                <button onClick={() => this.props.onSelectedBetRemoved(bet)}>
+                  X
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan='2'>
+              <button onClick={this.props.onSelectedBetsCleared}>Limpar</button>
+              <span className={style.toRight}>
+                Total:
+                <span className={style.bold}>
+                  {this.props.selectedBets.reduce(
+                    (prev, curr) => prev + curr.odd.value,
+                    0
+                  )}
+                </span>
+              </span>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    )
+  }
+}
+
+// Set default props
+Simulator.defaultProps = {
+  selectedBets: [],
+  onSelectedBetRemoved: () => {},
+  onSelectedBetsCleared: () => {}
+}
+
+export default Simulator
