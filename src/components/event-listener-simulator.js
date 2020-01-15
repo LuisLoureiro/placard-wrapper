789549000f5ed52648c2cb2fcc8b6ed7f4d1e2bd
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2020-01-15 00:59:21 +0000

    Fix simulator behaviour: - replace existing bet if clicked one is from the same event; - maintain the same position in the selected bets list;

--- src/components/event-listener-simulator.js
+++ src/components/event-listener-simulator.js
@@ -15,17 +15,30 @@ class EventListenerSimulator extends React.Component {
 
   componentDidMount () {
     if (this.state.eventName !== null) {
-      window.addEventListener(this.state.eventName, ({ detail }) =>
+      window.addEventListener(this.state.eventName, ({ detail }) => {
+        const betToInsert = {
+          code: detail.event.code,
+          home: detail.event.home,
+          away: detail.event.away,
+          name: detail.betType,
+          odd: detail.odd
+        }
+        const copyOfSelectedBets = this.state.selectedBets.concat()
+        const idx = copyOfSelectedBets.findIndex(
+          bet => bet.code === detail.event.code
+        )
+
+        if (idx !== -1) {
+          // replace existing one
+          copyOfSelectedBets.splice(idx, 1, betToInsert)
+        } else {
+          copyOfSelectedBets.push(betToInsert)
+        }
+
         this.setState({
-          selectedBets: this.state.selectedBets.concat({
-            code: detail.event.code,
-            home: detail.event.home,
-            away: detail.event.away,
-            name: detail.betType,
-            odd: detail.odd
-          })
+          selectedBets: copyOfSelectedBets
         })
-      )
+      })
     }
   }
 
